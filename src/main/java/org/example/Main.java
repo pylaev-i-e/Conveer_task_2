32f57151696bc0c4c0463d7c243f1d6272392136
ditrix <ditrix@tuta.io>
2025-12-07 19:15:21 +0300

    WEB

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
-import java.util.Scanner;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.time.LocalDate;
+import java.util.Optional;
 
 public class Main {
-    public static void main(String[] args) {
-        HelloWorld.main(args);
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/hello", exchange -> {
+            String query = Optional.ofNullable(exchange.getRequestURI().getQuery()).orElse("");
+            String name = "незнакомец";
+            if (query.startsWith("name=")) {
+                name = query.substring(5);
+            }
+            String response = "Привет, " + name + "! Сегодня: " + LocalDate.now();
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Сервер запущен на порту " + port);
     }
 }
